dfc31138727da0fa1c6f4c2c0206b8a65878434b
mrMaks2 <shpakovmaks@gmail.com>
2025-03-30 19:58:24 +0300

    Седьмой коммит (8.2)

---
+++ job/scraping/api/views.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+import datetime
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from scraping.models import City, Specialty, Vacancy
+
+from rest_framework.response import Response
+from rest_framework.decorators import action
+# from django.conf import settings
+from django.core.exceptions import ObjectDoesNotExist
+from .serializers import *
+
+TODAY = datetime.date.today()
+
+
+class CityListAPIView(viewsets.ModelViewSet):
+
+    queryset = City.objects.all()
+    serializer_class = CitySerializer
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+
+
+class SpecialtyListAPIView(viewsets.ModelViewSet):
+
+    queryset = Specialty.objects.all()
+    serializer_class = SpecialtySerializer
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+
+
+class VacancyListAPIView(viewsets.ModelViewSet):
+    """
+
+        ?city=kyiv&sp=python
+
+        city - slug for city filter;
+        specialty - filter for specialty;
+
+
+    """
+    queryset = Vacancy.objects.all()
+    serializer_class = VacancySerializer
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+
+    def get_queryset(self):
+        req = self.request
+        city_slug = req.query_params.get('city')
+        specialty_slug = req.query_params.get('sp')
+        date = req.query_params.get('date')
+        if date:
+            try:
+                date = datetime.datetime.strptime(date, '%Y-%m-%d')
+            except BaseException:
+                date = None
+        qs = None
+        period = TODAY - datetime.timedelta(1)
+        if city_slug and specialty_slug:
+            tm = {}
+            if date:
+                tm = {'timestamp': date}
+            else:
+                tm = {'timestamp__gte': period}
+            qs = Vacancy.objects.filter(
+                city__slug=city_slug, specialty__slug=specialty_slug, **tm)
+            if not qs.exists():
+                qs = Vacancy.objects.filter(
+                    city__slug=specialty_slug, specialty__slug=city_slug, **tm)
+
+        self.queryset = qs
+        return self.queryset
+
+# resp['result'][-1]['message']['chat']['id']
--- post_in/post_in/urls.py
+++ post_in/post_in/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework_simplejwt.views import (TokenObtainPairView, TokenRefreshView)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-    path('api-author/', include('rest_framework.urls', 'rest_framework'))
+    path('api-author/', include('rest_framework.urls', 'rest_framework')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
